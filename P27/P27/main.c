5d46219c96962d30d25ae26406d252a5e5e5d1f6
111360246 <t111360246@ntut.org.tw>
2022-10-24 14:25:41 +0800

    10/27

---
+++ P27/P27/main.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+long factorial(long number);
+
+int main(void)
+{
+	for (int i = 0; i <= 10; i++)
+	{
+		printf("%2d! = %1d\n", i, factorial(i));
+	}
+	system("pause");
+	return 0;
+}
+
+long factorial(long num)
+{
+	if (num <= 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return(num * factorial(num - 1));
+	}
+}
